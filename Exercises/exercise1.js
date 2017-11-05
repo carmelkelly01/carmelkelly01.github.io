dce00e8025576d8b45aaed6a19f6c7c431b00e7e
carmelkelly <carmelk@github.com>
2017-11-05 18:22:14 +0000

    Week 1

--- Exercises/exercise1.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello world');
-console.log('Hello again');
-console.log('I like typing this');
-console.log('This is fun');
-console.log('Yay!printing');
-console.log("I'd much rather you 'not'");
-console.log('I "said" do not touch this');
