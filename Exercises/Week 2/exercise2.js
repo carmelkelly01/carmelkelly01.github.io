9f7599301525c0e6cba39a5886c247dcffabeade
carmelkelly <carmelk@github.com>
2017-11-05 18:19:16 +0000

    Week 2

---
+++ Exercises/Week 2/exercise2.js
@@ -0,0 +1,35 @@
+var people = 30;
+var cars = 40;
+var buses = 15;
+
+//if there are more cars than people//
+if (cars > people)
+console.log ("We should take the cars.");
+
+//if there are less care than people//
+else if (cars < people)
+console.log ("We should not take the cars.");
+
+//if something else//
+else 
+console.log ("We can't decide");
+
+//if there are more buses than cars//
+if (buses > cars)
+console.log ("That's too many buses.");
+
+//if there are less buses than cars//
+else if (buses < cars)
+console.log ("Maybe we could take the buses.");
+
+//if not//
+else
+console.log ("We still can't decide.");
+
+//if there are more people than buses//
+if (people > buses)
+console.log ("Alright, let's just take the buses.");
+
+//if not//
+else 
+console.log ("Fine, let's stay home then.");
